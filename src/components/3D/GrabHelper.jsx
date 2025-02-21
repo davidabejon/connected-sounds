d9648f836831a42e64e38db7947fca9b3f0f873a
David Abejon Heras <david_abejon_heras@carrefour.com>
2025-02-21 11:17:30 +0100

    add. show GrabHelper after 10 seconds of inactivity

--- src/components/3D/GrabHelper.jsx
+++ src/components/3D/GrabHelper.jsx
@@ -3,19 +3,38 @@ import { PiHandGrabbingFill } from "react-icons/pi";
 import { FaArrowsAltH } from "react-icons/fa";
 import { useEffect, useState } from 'react';
 
+const IDLE_TIMEOUT = 10000;
+
 function GrabHelper({ startAnimation }) {
 
   const [hasUserInput, setHasUserInput] = useState(false)
+  const [isIdle, setIsIdle] = useState(false)
+
+  const resetTimer = () => {
+    setIsIdle(false)
+    setTimeout(() => {
+      setIsIdle(true)
+    }, IDLE_TIMEOUT);
+  };
 
   useEffect(() => {
     window.addEventListener('mousedown', () => {
       setHasUserInput(true)
     })
+    window.addEventListener('scroll', () => {
+      setHasUserInput(true)
+    })
   }, [])
-  
+
+  useEffect(() => {
+    if (!startAnimation) {
+      resetTimer()
+    }
+  }, [startAnimation])
+
   return (
     <>
-      <div className={`grab-helper ${startAnimation ? 'hidden' : hasUserInput ? 'hidden' : 'visible'}`}>
+      <div className={`grab-helper ${startAnimation ? 'hidden' : isIdle && !hasUserInput ? 'visible' : 'hidden'}`}>
         <FaArrowsAltH size={48} color='white' className='grab-icon1' />
         <PiHandGrabbingFill size={48} color='white' className='grab-icon2' />
         <p className='grab-text'>Grab the screen!</p>
