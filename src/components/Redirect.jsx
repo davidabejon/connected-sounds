36db352042bc543af2014c99bab8ccc0aa304653
davidabejon <71383097+davidabejon@users.noreply.github.com>
2025-04-22 18:45:06 +0200

    fix. use global variable for 3d path on Redirect

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
+import { newPath } from "../utilities";
 
-function Redirect({to = "/new"}) {
+function Redirect({to = newPath}) {
   useEffect(() => {
     window.location.href = to;
   })
